1d18449c39bff17cc9ab28a37028a99ace051b8e
sagarsharma21 <007sharma.sagar@gmail.com>
2023-09-19 14:54:06 +0530

    Add functionality to add or remove project-cards

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function toggleMenu() {
-    
-var toggleSwitch = document.getElementsByClassName('menu')[0];
+function toggleMenu() {    
+    var toggleSwitch = document.getElementsByClassName('menu')[0];
 
-if(toggleSwitch.style.display == ''){
-    toggleSwitch.style.display = 'none';
+    if(toggleSwitch.style.display == ''){
+        toggleSwitch.style.display = 'none';
+    }
+    else{
+        toggleSwitch.style.display = '';
+    }
 }
-else{
-    toggleSwitch.style.display = '';
-}
-}
+
+var projectListObject = [
+  { 
+    name: "Project One" 
+  },
+  {
+    name: "Project Two",
+  }
+];
+
+showProjects();
+
+function showProjects() {
+    
+    projectListObject.forEach(function (value, index) {
+        console.log(value);
+        var template = 
+        '<div class="project-card">'+
+            value.name+
+            '<ul>'+
+                '<li>Task 1</li>'+
+                '<li>Task 2</li>'+
+            '</ul>'+
+        '</div>';
+        document.getElementById('projectList').innerHTML += template; 
+        });
+    }
+
+    
+    function removeCards()  {
+        document.getElementById('projectList').innerHTML='none';
+    }
+
+// function showProject() {
+    
+//     var template = 
+//     '<div class="project-card">'+
+//         ' <span>Added dynamically</span>'+
+//         '<ul>'+
+//             '<li>Task 1</li>'+
+//             '<li>Task 2</li>'+
+//         '</ul>'+
+//     '</div>';
+//     document.getElementById('projectList').innerHTML += template; 
+// }
