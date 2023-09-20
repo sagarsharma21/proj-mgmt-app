87d2a0aec8714a872bccebed862408bef8053579
sagarsharma21 <007sharma.sagar@gmail.com>
2023-09-21 00:20:28 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function toggleMenu() {    
-    var toggleSwitch = document.getElementsByClassName('menu')[0];
+    function toggleMenu() {    
+        var toggleSwitch = document.getElementsByClassName('menu')[0];
 
-    if(toggleSwitch.style.display == ''){
-        toggleSwitch.style.display = 'none';
+        if(toggleSwitch.style.display == ''){
+            toggleSwitch.style.display = 'none';
+        }
+        else{
+            toggleSwitch.style.display = '';
+        }
     }
-    else{
-        toggleSwitch.style.display = '';
-    }
-}
 
 var projectListObject = [
   { 
@@ -18,20 +18,20 @@ var projectListObject = [
   }
 ];
 
-function showProjects(cardIdGen) {
+    function showProjects(cardIdGen) {
     
-    projectListObject.forEach(function (value, index) {
-        console.log(value);
-        var template = 
-        '<div class="project-card">'+ value.name+
-            '<ul>'+
-                '<li>Task 1</li>'+
-                '<li>Task 2</li>'+
-            '</ul>'+
-        '</div>';
+        projectListObject.forEach(function (value, index) {
+            console.log(value);
+            var template = 
+            '<div class="project-card">'+ value.name+
+                '<ul>'+
+                    '<li calss="task-list">Task 1</li>'+
+                    '<li class="task-list">Task 2</li>'+
+                '</ul>'+
+            '</div>';
 
-        document.getElementById(cardIdGen).innerHTML += template;
-        });
+            document.getElementById(cardIdGen).innerHTML += template;
+            });
     }
 
     
@@ -42,7 +42,11 @@ function showProjects(cardIdGen) {
 
     var idName = 1;
     function addBoard(id) {
-    
+            if (id.value.length < 5) {
+                alert("Board name can't be less than five characters!");
+                return;
+            } else {
+                
         idName++;
         console.log(idName );
 
@@ -51,7 +55,7 @@ function showProjects(cardIdGen) {
 
         var boardIdGen = 'board_'+idName ;
         var boardTemplate = '<section class="board-block" id='+boardIdGen+' style="margin-top: 5px; padding-top: 2px;">'+
-                                '<div>Board Name:'+id.value+'<button onclick="removeCards('+cardIdGen+')">Remove card</button></div>'+
+                                '<div>Board Name:'+id.value+'<button onclick="removeCards('+cardIdGen+')" id="removeCardBtn" >Remove card</button></div>'+
                                 '<div class="project-block" style="border: 2px solid rgb(130, 185, 20); margin :2px; padding: 2px;"'+
                                 'id='+ cardIdGen+'>'+
                                 '</div>'+
@@ -59,7 +63,11 @@ function showProjects(cardIdGen) {
         document.getElementById('boardBlockList').innerHTML += boardTemplate;
         showProjects(cardIdGen);
         console.log(id.value+" bugfix")
-        document.getElementById('menu-list').innerHTML += '<li onclick="loadMenu('+boardIdGen+')">'+id.value+'</li>';
+
+        document.getElementById('menu-list').innerHTML += '<li onclick="loadMenu('+boardIdGen+')">'+id.value+'<hr></li>';
+                  alert("Board name" + id.value + "added!");
+                  id.value=id.defaultValue;  
+                    }
     }
 
     function loadMenu(element) {
